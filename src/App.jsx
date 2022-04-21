f84c5f98b48c6b7d46cf7ab6163c42ba4ceafa50
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-04-21 10:15:16 +0900

    ✨ pagesとルートの作成

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+// pages & components
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import Signup from "./pages/signup/Signup";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
