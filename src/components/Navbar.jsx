9e24f65a276c135f2d2a5751d6f627df34cf4d51
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-04-21 13:58:44 +0900

    ✨ ログアウトフックの使用

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
 
 // styles
 import styles from "./Navbar.module.css";
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -14,6 +17,12 @@ export default function Navbar() {
         </li>
         <li>
           <Link to="/signup">Signup</Link>
+        </li>
+
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
