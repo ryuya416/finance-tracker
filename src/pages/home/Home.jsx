253abcd594a9a5cf7052167d6035a3dd0f2d3ee2
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-04-22 13:43:59 +0900

    ✨ 取引フォームの作成

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,16 @@
 // styles
 import styles from "./Home.module.css";
 
+// components
+import TransactionForm from "./TransactionForm";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.content}>transaction list</div>
+      <div className={styles.sidebar}>
+        <TransactionForm />
+      </div>
+    </div>
+  );
 }
